cd4a4e6027e36e4f7085d59060821fd03599c414
hamsoter <omilzmil0w0@gmail.com>
2022-02-23 17:38:25 +0900

    jdbc 레포지토리 구현

--- src/main/java/otaku/shelterforcowards/SpringConfig.java
+++ src/main/java/otaku/shelterforcowards/SpringConfig.java
@@ -1,13 +1,23 @@
 package otaku.shelterforcowards;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import otaku.shelterforcowards.repository.MemberRepository;
-import otaku.shelterforcowards.repository.MemoryMemberRepository;
+import otaku.shelterforcowards.repository.jdbcMemberRepository;
 import otaku.shelterforcowards.service.MemberService;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +26,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new jdbcMemberRepository(dataSource);
     }
 }
